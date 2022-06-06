a5c49e37c040d4fe9c9f9ac546b66869b8b23ba2
Ciuff4 <gasperini_cristina@libero.it>
2022-06-06 16:37:51 +0200

    stampa paragrafo con parola censurata + lunghezza

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 $pharagraph='The ring-necked dove (Streptopelia capicola), also known as the Cape turtle dove or half-collared dove, is a widespread and often abundant dove species in East and southern Africa. It is a mostly sedentary bird, found in a variety of open habitats. Within range, its penetrating and rhythmic, three-syllabled crooning is a familiar sound at any time of the year. Its name is derived from the semi-collar of black feathers on the lower nape, a feature shared with a number of Streptopelia species. Like all doves, they depend on surface water. They congregate in large flocks at waterholes in dry regions to drink and bathe.';
 $pharagraph_lenght= strlen($pharagraph);
+$bad_word=$_GET['parola'];
+$frase_censurata= str_replace($bad_word,'***',$pharagraph);
+
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +17,6 @@ $pharagraph_lenght= strlen($pharagraph);
 <body>
     <h1>The ring-necked dove</h1>
     <span style='display:block'> Il seguente paragrafo contiene: <?php echo $pharagraph_lenght?> parole</span>
-    <p><?php echo $pharagraph ?> </p>
+    <p><?php echo $frase_censurata ?> </p>
 </body>
 </html>
